fcba6566240e91db933267df4dc47b3d79e89b8d
ted6311 <theodor.lignell@gmail.com>
2026-02-16 13:58:27 +0100

    class res and voltage-source

--- components.py
+++ components.py
@@ -11,6 +11,42 @@ class ElectricalComponent(ABC):
     def apply(self, A, b, system_mAp, frequency):
         pass
 
-    def apply_voltage(self, solution, system_mAp):
-        n1, n2 = nodes
-        
+    def get_voltage(self, solution, system_mAp):
+        n1, n2 = self.nodes
+        v1 = solution[system_mAp[f"V{n1}"]]
+        v2 = solution[system_mAp[f"V{n2}"]]
+        return v1 - v2
+
+    def get_currrent(self, solution, system_mAp):
+        pass
+
+
+
+class Resistor(ElectricalComponent):
+    def apply(self, A, b, system_mAp, frequency):
+        v1 = system_mAp[f"V{self.nodes[0]}"]
+        v2 = system_mAp[f"V{self.nodes[1]}"]
+        i = system_mAp[f"I_{self.name}"]
+
+        R = self.value
+
+        # Ohm law
+        A[i, v1] = 1
+        A[i, v2] = -1
+        A[i, i] = -R
+    def get_current(self, solution, system_mAp):
+        i = system_mAp[f"I_{self.name}"
+        return solution[i]
+
+class Voltage(ElectricalComponent):
+    def apply(self, A, b, system_mAp, frequency):
+        v_p = system_mAp[f"V{self.nodes[0]}"]
+        v_m = system_mAp[f"V{self.nodes[1]}"]
+        i = system_mAp[f"I_{self.name}"]
+
+        A[i, v_p] = 1
+        A[i, v_m] = -1
+        b[i] = self.value
+
+    def get_current(self, solution, system_mAp):
+        return solution[system_mAp[f"I_{self.name}"]]
