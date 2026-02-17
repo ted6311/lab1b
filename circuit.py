0cf889f52a5a58d38aaa7c0c16d5e8aa19ad388e
ted6311 <theodor.lignell@gmail.com>
2026-02-17 09:27:20 +0100

    add_comp fixed

--- circuit.py
+++ circuit.py
@@ -2,8 +2,19 @@ import numpy as np
 from components import Ground
 
 class Circuit:
-    def __init__(self, frequency=0):
+    def __init__(self, frequency=0.0):
         self.frequency = frequency
         self.components = []
         self.system_map = {}
+        self.system_matrix = None
+        self. system_rhs = None
         self.solution = None
+    def add_component(self, component):
+        if component not in self.components:
+            self.components.append(component)
+        else:
+            raise ValueError("Component {} already defined".format(component.name))
+
+
+
+
